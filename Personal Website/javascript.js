ea95debb430d0fe494b643053325b8b5ba4f621f
Alexandermed219 <alexandermed219@gmail.com>
2023-04-15 18:12:28 -0400

    Form attempt

--- Personal Website/javascript.js
+++ Personal Website/javascript.js
@@ -1,20 +1,9 @@
-const form = document.querySelector("#form")
-   const submitButton = document.querySelector("#submit")
-   const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+var form = document.getElementById('form')
 
-   form.addEventListener('submit', e => {
-     submitButton.disabled = true
-     e.preventDefault()
-     let requestBody = new FormData(form)
-     fetch(scriptURL, { method: 'POST', body: requestBody})
-       .then(response => {
-          alert('Success!', response)
-          submitButton.disabled = false
-         })
-       .catch(error => {
-       alert('Error!', error.message)
-         submitButton.disabled = false
-
-       }
-       )
-   })
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+})
+var name = document.getElementById('name').value
+var name2 =document.getElementById('name2').value
+var email = document.getElementById('email').value
+alert("Registered");
